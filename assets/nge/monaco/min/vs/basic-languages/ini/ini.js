887932a1f2b971737b0c4736fdd0338b2e8d4558
Mamadou Cisse <mciissee@gmail.com>
2023-09-11 20:11:33 +0200

    Auto-generated commit [ci skip]

--- assets/nge/monaco/min/vs/basic-languages/ini/ini.js
+++ assets/nge/monaco/min/vs/basic-languages/ini/ini.js
@@ -1,6 +1,6 @@
 "use strict";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
+ * Version: 0.43.0(94c055bcbdd49f04a0fa15515e848542a79fb948)
  * Released under the MIT license
  * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
  *-----------------------------------------------------------------------------*/
