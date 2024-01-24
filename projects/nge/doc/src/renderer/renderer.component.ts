9e7dd2571139220872140e8e3e2a67a0ef967ea0
Mamadou Cisse <mciissee@gmail.com>
2024-01-24 23:18:12 +0100

    refactor(doc): reuse markdown renderer

--- projects/nge/doc/src/renderer/renderer.component.ts
+++ projects/nge/doc/src/renderer/renderer.component.ts
@@ -5,9 +5,11 @@ import {
   Injector,
   OnDestroy,
   OnInit,
+  SimpleChanges,
   Type,
   ViewChild,
-  ViewContainerRef
+  ViewContainerRef,
+  inject
 } from '@angular/core';
 import { CompilerService } from '@cisstech/nge/services';
 import { Subscription, firstValueFrom } from 'rxjs';
@@ -21,25 +23,22 @@ import { NgeDocService } from '../nge-doc.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NgeDocRendererComponent implements OnInit, OnDestroy {
+  private readonly injector = inject(Injector);
+  private readonly renderers = inject(NGE_DOC_RENDERERS);
+  private readonly docService = inject(NgeDocService)
+  private readonly compilerService = inject(CompilerService)
+  private readonly changeDetectorRef = inject(ChangeDetectorRef)
+
   private subscriptions: Subscription[] = [];
-  private markdownRenderer?: Type<any>;
+  protected loading = false;
+  protected noFound = false;
+  protected componentRefByTypes = new Map<Type<any>, ComponentRef<any>>();
+
+  componentRef?: ComponentRef<any>;
 
   @ViewChild('container', { read: ViewContainerRef, static: true })
   container!: ViewContainerRef;
 
-  loading = false;
-  component?: ComponentRef<any>;
-
-  get notFound(): boolean {
-    return !this.loading && !this.component;
-  }
-
-  constructor(
-    private readonly injector: Injector,
-    private readonly docService: NgeDocService,
-    private readonly compilerService: CompilerService,
-    private readonly changeDetectorRef: ChangeDetectorRef
-  ) { }
 
   ngOnInit(): void {
     this.subscriptions.push(
@@ -50,37 +49,6 @@ export class NgeDocRendererComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.clearViewContainer();
     this.subscriptions.forEach((s) => s.unsubscribe());
-  }
-
-  private async onChangeState(state: NgeDocState): Promise<void> {
-    try {
-      this.clearViewContainer();
-
-      this.showLoading();
-
-      let component: ComponentRef<any> | undefined;
-      if (state.currLink) {
-        const renderer = await state.currLink.renderer;
-        switch (typeof renderer) {
-          case 'string':
-            component = await this.rendererMarkdown(renderer);
-            break;
-          case 'function':
-            component = await this.compilerService.render({
-              type: await renderer(),
-              inputs: state.currLink.inputs,
-              container: this.container,
-            });
-            break;
-        }
-      }
-      this.component = component;
-    } catch (error) {
-      console.error(error);
-    } finally {
-      this.loading = false;
-      this.changeDetectorRef.markForCheck();
-    }
   }
 
   private showLoading(): void {
@@ -96,55 +64,123 @@ export class NgeDocRendererComponent implements OnInit, OnDestroy {
     }, 1000);
   }
 
-  private async rendererMarkdown(data: string): Promise<ComponentRef<any>> {
-    const renderers = this.injector.get(NGE_DOC_RENDERERS);
-    if (!renderers?.markdown) {
+  private clearViewContainer(): void {
+    const componentRefs = Array.from(this.componentRefByTypes.values());
+    if (this.componentRef && componentRefs.includes(this.componentRef)) {
+      while (this.container.length > 0) {
+        this.container.detach()
+      }
+    } else {
+      this.componentRef?.destroy();
+      this.componentRef = undefined;
+      this.container.clear();
+    }
+  }
+
+  private async onChangeState(state: NgeDocState): Promise<void> {
+    try {
+      this.showLoading();
+      this.clearViewContainer();
+      if (state.currLink) {
+        const renderer = await state.currLink.renderer;
+        switch (typeof renderer) {
+          case 'string':
+            await this.renderMarkdown(renderer);
+            break;
+          case 'function':
+            this.componentRef = await this.compilerService.render({
+              type: await renderer(),
+              inputs: state.currLink.inputs,
+              container: this.container,
+            });
+            break;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.loading = false;
+      this.noFound = !this.componentRef;
+      this.changeDetectorRef.markForCheck();
+    }
+  }
+
+
+  private async renderMarkdown(data: string): Promise<void> {
+    if (!this.renderers?.markdown) {
       throw new Error('[nge-doc]: missing markdown renderer.');
     }
 
-    const renderer = renderers.markdown;
-    let inputs: Record<string, any> = {
-      data, // we assume that data is a markdown content.
-    };
+    const renderer = this.renderers.markdown;
+    const type = await renderer.component();
 
-    if (!data.includes('\n')) {
-      // if data does not include at least two lines then it's an url
-      const http = this.injector.get(HttpClient, null);
-      if (!http) {
-        throw new Error(
-          '[nge-doc] When using the `file` renderer you *have to* pass the `HttpClient` as a parameter of the `forRoot` method. See README for more information'
-        );
+    const createInputs = async (): Promise<Record<string, any>> => {
+      let inputs: Record<string, any> = {
+        data, // we assume that data is a markdown content.
+      };
+
+      if (!data.includes('\n')) {
+        // if data does not include at least two lines then it's an url
+        const http = this.injector.get(HttpClient, null);
+        if (!http) {
+          throw new Error(
+            '[nge-doc] When using the `file` renderer you *have to* pass the `HttpClient` as a parameter of the `forRoot` method. See README for more information'
+          );
+        }
+
+        inputs = {
+          data: await firstValueFrom(http.get(data, { responseType: 'text' })),
+        };
       }
 
-      inputs = {
-        data: await firstValueFrom(http.get(data, { responseType: 'text' })),
-      };
+      let customInputs: Record<string, any> = {};
+      if (typeof renderer.inputs === 'function') {
+        customInputs = await renderer.inputs(this.injector);
+      } else if (typeof renderer.inputs === 'object') {
+        customInputs = renderer.inputs;
+      }
+
+      return { ...customInputs, ...inputs };
     }
 
-    let customInputs: Record<string, any> = {};
-    if (typeof renderer.inputs === 'function') {
-      customInputs = await renderer.inputs(this.injector);
-    } else if (typeof renderer.inputs === 'object') {
-      customInputs = renderer.inputs;
+    const markdownComponent = this.componentRefByTypes.get(type);
+    if (markdownComponent) {
+      this.attachComponent(markdownComponent, await createInputs());
+      return;
     }
 
-    if (!this.markdownRenderer) {
-      this.markdownRenderer = await renderer.component();
-    }
-
-    return this.compilerService.render({
-      inputs: {
-        ...customInputs,
-        ...inputs,
-      },
-      type: this.markdownRenderer,
+    const componentRef = await this.compilerService.render({
+      type,
+      inputs: await createInputs(),
       container: this.container,
     });
+
+    this.componentRef = componentRef;
+    this.componentRefByTypes.set(type, componentRef);
   }
 
-  private clearViewContainer(): void {
-    this.component?.destroy();
-    this.component = undefined;
-    this.container.clear();
+  private async attachComponent(componentRef: ComponentRef<any>, inputs: Record<string, any>): Promise<void> {
+    this.container.insert(componentRef.hostView);
+    this.componentRef = componentRef;
+
+    // compute changes
+    const changes: SimpleChanges = {};
+    const { instance, changeDetectorRef } = componentRef;
+    Object.keys(inputs).forEach((key) => {
+      changes[key] = {
+        currentValue: inputs[key],
+        previousValue: instance[key],
+        firstChange: false,
+        isFirstChange: () => false,
+      };
+      instance[key] = inputs[key];
+    });
+
+    // call ngOnChanges
+    if (instance.ngOnChanges) {
+      await instance.ngOnChanges(changes);
+    }
+
+    changeDetectorRef.markForCheck();
   }
 }
