030a3112799579407a08ac8fced4c0cf97d8d86a
Mamadou Cisse <mciissee@gmail.com>
2024-08-26 21:20:45 +0200

    Auto-generated commit

---
+++ chunk-FTR4GWYH.js
@@ -0,0 +1,1 @@
+import{a,b,c,d}from"./chunk-G2LWV6XQ.js";import"./chunk-6YRNS2CN.js";import"./chunk-4STL355D.js";export{a as NGE_DOC_RENDERERS,d as NgeDocModule,c as extractNgeDocSettings,b as isNgeDocSettings};
---
+++ chunk-YOKUGWMH.js
@@ -0,0 +1,90 @@
+import{B as d,C as l}from"./chunk-QET4ZBO7.js";import{Aa as a,Fb as g,J as c,N as m,O as h,Ra as e,qb as p,ya as s,za as i}from"./chunk-4STL355D.js";var u=(()=>{let t=class t{constructor(){}ngOnInit(){}};t.\u0275fac=function(o){return new(o||t)},t.\u0275cmp=m({type:t,selectors:[["app-home"]],decls:18,vars:0,consts:[["align","center"],["src","./assets/images/nge.svg","alt","Logo NGE","width","120px"],["href","https://angular.io"]],template:function(o,f){o&1&&(s(0,"nge-markdown"),e(1,`
+`),s(2,"h1",0),e(3," NG Essentials"),i(),e(4,`
+
+`),s(5,"div",0),e(6,`
+  `),a(7,"img",1),e(8,`
+`),i(),e(9,`
+
+`),s(10,"div",0),e(11,`
+
+NG Essentials is a collection of libraries for `),s(12,"a",2),e(13,"Angular"),i(),e(14,` developers.
+
+[![Tests](https://github.com/cisstech/nge/actions/workflows/tests.yml/badge.svg)](https://github.com/cisstech/nge/actions/workflows/tests.yml)
+
+[![codecov](https://codecov.io/gh/cisstech/nge/branch/main/graph/badge.svg)](https://codecov.io/gh/cisstech/nge)
+[![codefactor](https://www.codefactor.io/repository/github/cisstech/nge/badge/main)](https://www.codefactor.io/repository/github/cisstech/nge/overview/main)
+[![GitHub Tags](https://img.shields.io/github/tag/cisstech/nge.svg)](https://github.com/cisstech/nge/tags)
+[![npm package](https://img.shields.io/npm/v/@cisstech/nge.svg)](https://www.npmjs.org/package/@cisstech/nge)
+[![NPM downloads](http://img.shields.io/npm/dm/@cisstech/nge.svg)](https://npmjs.org/package/@cisstech/nge)
+[![licence](https://img.shields.io/github/license/cisstech/nge)](https://github.com/cisstech/nge/blob/main/LICENSE)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+`),i(),e(15,`
+
+## \u2728 Features Guides
+
+Follow the guides to integrate these essentials features into your applications:
+
+* [nge-doc](docs/nge-doc/)
+
+Project documentation tool.
+
+* [nge-markdown](docs/nge-markdown/)
+
+Markdown parser library based on [Marked](https://github.com/markedjs/marked) \`(v^11)\`.
+
+* [nge-monaco](docs/nge-monaco/)
+
+[Monaco Editor](https://microsoft.github.io/monaco-editor/) integration in angular.
+
+## \u{1F4E6} Installation
+
+\`\`\`bash
+npm install @cisstech/nge
+\`\`\`
+
+## \u2328\uFE0F Development
+
+* Clone and install
+
+\`\`\`bash
+git clone https://github.com/cisstech/nge
+cd nge
+npm install
+\`\`\`
+
+* Serve demo
+
+\`\`\`bash
+npm run start
+\`\`\`
+
+Browser would open automatically at <//localhost:4200/>.
+
+## \u{1F91D} Contribution
+
+Contributions are always welcome. `),a(16,"br"),e(17,`
+
+Please read our [CONTRIBUTING.md](https://github.com/cisstech/nge/blob/main/CONTRIBUTING.md) first. You can submit any ideas as [pull requests](https://github.com/cisstech/nge/pulls) or as [GitHub issues](https://github.com/cisstech/nge/issues).
+
+Please just make sure that ...
+
+Your code style matches with the rest of the project
+
+Unit tests pass
+
+Linter passes
+
+## \u2753 Support Development
+
+The use of this library is totally free.
+
+As the owner and primary maintainer of this project, I am putting a lot of time and effort beside my job, my family and my private time to bring the best support I can by answering questions, addressing issues and improving the library to provide more and more features over time.
+
+If this project has been useful, that it helped you or your business to save precious time, don't hesitate to give it a star to support its maintenance and future development.
+
+## \u2728 License
+
+MIT \xA9 [Mamadou Cisse](https://github.com/cisstech)
+
+`),i())},dependencies:[d],styles:["[_nghost-%COMP%]{display:block;width:100vw;height:100vh;padding:2rem 8rem;box-sizing:border-box}@media only screen and (max-width: 600px){[_nghost-%COMP%]{padding:2rem}}"]});let n=t;return n})();var M=(()=>{let t=class t{};t.\u0275fac=function(o){return new(o||t)},t.\u0275mod=h({type:t}),t.\u0275inj=c({imports:[p,l,g.forChild([{path:"",component:u}])]});let n=t;return n})();export{M as HomeModule};
