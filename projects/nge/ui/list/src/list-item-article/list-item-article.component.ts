30eb4b9c7c5aceba3e161e3a4b4317e6bf8b9afc
Mamadou Cisse <mciissee@gmail.com>
2025-11-07 18:24:59 +0100

    feat(ui): set list item article host clickable

--- projects/nge/ui/list/src/list-item-article/list-item-article.component.ts
+++ projects/nge/ui/list/src/list-item-article/list-item-article.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core'
 import { ListItemTag } from '../list'
 
@@ -20,13 +21,41 @@ export class ListItemArticleComponent implements OnInit {
   @Input() articleIconTemplate?: TemplateRef<any>
   @Input() articleTagIconTemplate?: TemplateRef<{ text: string; data?: any }>
 
+  @Output() didClick = new EventEmitter()
   @Output() didClickTag = new EventEmitter<string>()
-  @Output() didClickTagItem = new EventEmitter<ListItemTag>()
   @Output() didClickTitle = new EventEmitter()
+  @Output() didClickTagItem = new EventEmitter<ListItemTag>()
 
-  protected isTagsCliclable = false
+  protected isClickable = false
+  protected isTagsClickable = false
+  protected isTitleClickable = false
 
   ngOnInit(): void {
-    this.isTagsCliclable = this.didClickTag.observed || this.didClickTagItem.observed
+    this.isClickable = this.didClick.observed
+    this.isTitleClickable = this.didClickTitle.observed
+    this.isTagsClickable = this.didClickTag.observed || this.didClickTagItem.observed
   }
+
+  protected onClickHost() {
+    this.didClick.emit()
+  }
+
+  protected onClickTitle($event: Event) {
+    $event.stopPropagation()
+    $event.preventDefault()
+    this.didClickTitle.emit()
+  }
+
+  protected onClickTag($event: Event, tag: string) {
+    $event.stopPropagation()
+    $event.preventDefault()
+    this.didClickTag.emit(tag)
+  }
+
+  protected onClickTagItem($event: Event, tag: ListItemTag) {
+    $event.stopPropagation()
+    $event.preventDefault()
+    this.didClickTagItem.emit(tag)
+  }
+
 }
